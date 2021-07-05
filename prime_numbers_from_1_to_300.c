6cde730aec3231fe12a3c643d7757d7cfb4962a1
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-07-05 14:40:14 +0530

    Update prime_numbers_from_1_to_300.c

--- prime_numbers_from_1_to_300.c
+++ prime_numbers_from_1_to_300.c
@@ -5,14 +5,14 @@ int main()
 {
     int num,i;
     for(num=1;num<=300;num++){
-        for(i=2;i<num;i++){
-            if(num%i==0){
-                break;
-            }
-            else
-            printf("%d\n",num);
+        for(i=2;i<=num;i++){
+            if(num%i==0)
             break;
         }
+        if(i==num){
+        printf("%d\n",num);
+    }
+        
     }
 
     return 0;

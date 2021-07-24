7ecfe66f33bad7569ddb459d406b5ae77aabf5a9
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-07-24 12:59:33 +0530

    Create use_of_functions.c

---
+++ use_of_functions.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void are();
+void maaa();
+void chudao();
+int main()
+{
+    printf("legendry gaali :");
+    dev();
+    saumya();
+    love();
+    return 0;
+}
+void dev()
+{
+    printf("are ");
+}
+void saumya()
+{
+    printf("maa ");
+}
+void love()
+{
+    printf("chudao");
+}

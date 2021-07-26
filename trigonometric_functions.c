ea6ce72a91af8ec3cf095553e115cde6f747012c
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-07-27 00:16:00 +0530

    Create trigonometric_functions.c

---
+++ trigonometric_functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    float a=0.5,w,x,y,z;
+    w=sin(a);
+    x=cos(a);
+    y=tan(a);
+    z=pow(a,2);
+    printf("%f %f %f %f",w,x,y,z);
+    return 0;
+}

414a13ea869b2caa08ee816c10f76696cbbd67c7
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-08-22 00:00:39 +0530

    Create natural_number_sum.c

---
+++ natural_number_sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int sum(int);
+int main() 
+{
+    int num,q;
+    printf("Enter the number till you wsant the sum :");
+    scanf("%d",&num);
+    q=sum(num);
+    printf("The sum from 1 to %d is:%d",num,q);
+    
+    return 0;
+}
+int sum(int n)
+{
+    if(n==0){
+        return(n);
+    }
+    else{
+        return (n+sum(n-1));
+    }
+}

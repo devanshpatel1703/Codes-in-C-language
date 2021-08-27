41fc7debb94a5c6ccee43bd5283ae441362ada20
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-08-27 22:11:02 +0530

    Update hfc_by_recurtion.c

--- hfc_by_recurtion.c
+++ hfc_by_recurtion.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 int hcf(int,int,int);
-int main() 
+int main()
 {
-    int n1,n2,gcd,r,t;
+    int n1,n2,gcd,t;
     printf("Enter 2 numbers to find HCF :");
     scanf("%d %d",&n1,&n2);
-    if(n1<n2){
-        t=n1;
-        n1=n2;
-        n2=t;
+    if(n1>n2){
+    gcd=hcf(n1,n2);
     }
-    gcd=hcf(n1,n2,r);
+    else{
+    gcd = hcf(n2,n1);
+    }
     printf("The HCF is :%d",gcd);
     return 0;
 }
-int hcf(int n1,int n2,int r)
+int hcf(int n1,int n2)
 {
-    if(n2%n1!=0){
-        r=n2%n1;
-        hcf(r,n1,n2%n1);
-        return(r);
+    if(n1%n2==0){
+        return(n2);
     }
+    r=n1%n2;
+    hcf(n2,r);
+    
 }
 
 

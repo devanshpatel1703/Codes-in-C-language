5983c7a090fec54730f866d6d7d3a5a64d15ed16
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-08-30 13:30:09 +0530

    Create devansh_by_stars.c

---
+++ devansh_by_stars.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{   
+    printf("***\n*  *\n*   *\n*  *\n***\n\n\n");//for D
+    printf("****\n*\n*\n****\n*\n*\n****\n\n\n");//for E
+    printf("*     *\n *   *\n  * *\n   *\n\n\n");//for V
+    printf("    *\n   * *\n  * * *\n *     *\n*       *\n\n\n");//for A
+    printf("*    *\n* *  *\n*  * *\n*    *\n\n\n");//for N
+    printf("  *\n *\n* * *\n   *\n  *\n\n\n");//for S
+    printf("*    *\n*    *\n******\n*    *\n*    *\n\n\n");//for H
+    return 0;
+}

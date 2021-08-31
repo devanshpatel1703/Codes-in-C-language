a21a44c2e542595f5a291feddce75ef68d3a1bf6
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-08-31 19:02:51 +0530

    Create sum_of_all_the_non_multiples_of_17_in_a_range.c

---
+++ sum_of_all_the_non_multiples_of_17_in_a_range.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    int n1,n2,i=1,sum=0;
+    printf("Enter the interval in wich you want sum of non-17 multiplies :");
+    scanf("%d %d",&n1,&n2);
+    for(i=n1;i<=n2;i++){
+        if(i%17!=0){
+            printf("%d ",i);
+            sum=sum+i;
+        }
+    }
+    printf("\nSum of all the numbers in the range which are not multiple of 17 is :%d",sum);
+    return 0;
+}

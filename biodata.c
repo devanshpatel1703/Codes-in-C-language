fa318db42a9814fb5c988f10f96448e85e71c9e1
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-09-20 21:10:47 +0530

    Create biodata.c

---
+++ biodata.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    printf("My name is Devansh\n");
+    printf("I have done my 12 Form st.Xaviers\n");
+    printf("I got 93 percent in class 12th\n");
+    printf("I am currently Persuing B.Tech from GLA Mathura");
+    return 0;
+}

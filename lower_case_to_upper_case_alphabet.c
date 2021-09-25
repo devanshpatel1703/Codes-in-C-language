c75fcab64b936f4883f72b188611e879253e0a01
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-09-25 18:48:31 +0530

    Create lower_case_to_upper_case_alphabet.c

---
+++ lower_case_to_upper_case_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() 
+{
+    char ch;
+    printf("Enter any lower case alphabet :");
+    scanf("%c",&ch);
+    printf("The upper case is :%c",ch-32);
+    return 0;
+}

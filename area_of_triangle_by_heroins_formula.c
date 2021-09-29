acd622a01226ea2ed335f0356a38416114a031cf
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-09-29 21:46:19 +0530

    Create area_of_triangle_by_heroins_formula.c

---
+++ area_of_triangle_by_heroins_formula.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float a,b,c,s,A;
+printf("Enter three numbers :");
+scanf("%f %f %f", &a,&b,&c);
+s=(a+b+c)/2;
+A=sqrt(s*(s-a)*(s-b)*(s-c));
+printf("%f",A);
+return 0;
+}

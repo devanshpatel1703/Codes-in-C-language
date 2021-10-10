3545f74cf7940da6a4c96b8167f4b6118b19af78
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-10-10 18:25:29 +0530

    Create age_for_voting_by_ternary.c

---
+++ age_for_voting_by_ternary.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    printf("enter the value of a :");
+    scanf("%d",&a);
+    (a>=18)?printf("You can vote.\n"):printf("You cant vote.");
+    return 0;
+}

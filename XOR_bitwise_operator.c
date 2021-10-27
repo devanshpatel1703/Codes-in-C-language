a7d05cf4d7282abf8e7fc26cd7674a41f5b9d69f
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-10-27 23:26:30 +0530

    Create XOR_bitwise_operator.c

---
+++ XOR_bitwise_operator.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  
+int main()  
+{  
+   int a=32,b=26;  // variable declarations  
+   printf("The output of the Bitwise exclusive OR operator a^b is %d",a^b);  
+   return 0;  
+}  

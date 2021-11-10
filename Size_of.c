76415a99de30f2457349ba6a4446f9758692915e
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-11-10 23:41:26 +0530

    Create Size_of.c

---
+++ Size_of.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int i;
+
+    float f;
+
+    double d;
+
+    long double l;
+
+    short int s;
+
+    char c;
+
+    printf("size of int = %ld\n",sizeof(i));
+
+    printf("size of float = %ld\n",sizeof(f));
+
+    printf("size of char = %ld\n",sizeof(c));
+
+    printf("size of double = %ld\n",sizeof(d));
+
+    printf("size of long double = %ld\n",sizeof(l));
+
+    printf("size of short int = %ld\n",sizeof(s));
+
+    return 0;
+
+}

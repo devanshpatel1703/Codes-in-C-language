eeebd1d77a3522622dfba11fab168fbe09ddb795
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-11-30 21:21:48 +0530

    Create switch_output.c

---
+++ switch_output.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+    void main()
+    {
+       int i=0;
+       switch(i)
+       {
+           case '0':printf("yes");
+           break;
+           case '1':printf("No");
+           break;
+           default:printf("yjhg");
+       }
+    }

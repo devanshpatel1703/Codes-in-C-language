f0a59688c2c07b694ccef3e9297f051ca2949d9f
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2021-12-28 23:24:37 +0530

    Create scan_and_print_string.c

---
+++ scan_and_print_string.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    char array[25];
+    printf("Enter your name as string :");
+    scanf("%s",&array);
+    printf("Your name is : %s",array);
+    return 0;
+}

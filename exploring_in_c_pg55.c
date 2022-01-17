7028056cccb4ff710275fe4e005789022f7cc8e8
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-01-17 19:28:46 +0530

    Create exploring_in_c_pg55.c

---
+++ exploring_in_c_pg55.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+int main() 
+{
+    int customer,product;
+    float amount;
+    printf("Choose which type of costomer you are :\n");
+    printf("1.For Industrial purchase.\n");
+    printf("2.For Governmental purchase.\n");
+    printf("3.For University purchase.\n");
+    printf("Your type here : ");
+    scanf("%d",&customer);
+    printf("Choose the type of product you want.\n");
+    printf("1.For 8-bit.\n");
+    printf("2.For 16-bit.\n");
+    printf("3.For 32-bit.\n");
+    printf("Your purchase type here : ");
+    scanf("%d",&product);
+    printf("Enter the amount of purchase :");
+    scanf("%f",&amount);
+    if(product==3){
+        if(amount<50000){
+            if(customer==1){
+                printf("The amount you have to pay is : %f",amount*0.05);
+            }
+            else if(customer==2){
+                printf("The amount you have to pay is : %f",amount*0.065);
+            }
+            else if(customer==3){
+                printf("The amount you have to pay is : %f",amount*0.075);
+            }
+        }
+        else if(amount>=50000 && amount<100000){
+            if(customer==1){
+                printf("The amount you have to pay is : %f",amount*0.075);
+            }
+            else if(customer==2){
+                printf("The amount you have to pay is : %f",amount*0.085);
+            }
+            else if(customer==3){
+                printf("The amount you have to pay is : %f",amount*0.075);
+            }
+        }
+        else if(amount>=100000){
+            if(customer==1){
+                printf("The amount you have to pay is : %f",amount*0.1);
+            }
+            else if(customer==2){
+                printf("The amount you have to pay is : %f",amount*0.1);
+            }
+            else if(customer==3){
+                printf("The amount you have to pay is : %f",amount*0.075);
+            }
+        }
+    }
+    else if(product==2){
+        if(amount<10000){
+            printf("You get no discount at all.");
+        }
+        else if(amount>=10000){
+            if(customer==1){
+                printf("The amount you have to pay is : %f",amount*0.5);
+            }
+            else if(customer==2){
+                printf("The amount you have to pay is : %f",amount*0.6);
+            }
+            else if(customer==3){
+                printf("The amount you have to pay is : %f",amount*0.5);
+            }
+        }
+    }
+    else{
+        printf("The amount you have to pay is : %f",amount*0.1);
+    }
+    return 0;
+}

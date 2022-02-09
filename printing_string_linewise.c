f0fa155d5344027fafa4e8ade0993f21c0bcd931
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-02-09 19:14:24 +0530

    Create printing_string_linewise.c

---
+++ printing_string_linewise.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    int i;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    for(i=0;i<strlen(s);i++){
+        if(s[i]==' '){
+            printf("\n");
+        }
+        else{
+            printf("%c",s[i]);
+        }
+    }
+    return 0;
+}

746513634257b8e69067b57399d33984c460d754
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-02-25 02:11:05 +0530

    Create turning_on_a_specific_bit_in_one_byte_integer.c

---
+++ turning_on_a_specific_bit_in_one_byte_integer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    int num,b;
+    printf("Enter any number (0-255) :");
+    scanf("%d",&num);
+    printf("Enter the bit you want to turn on :");
+    scanf("%d",&b);
+    b=pow(2,b-1);
+    printf("The result after turning on the entred bit is : %d",num|b);
+    return 0;
+}

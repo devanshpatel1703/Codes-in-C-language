3ec5d8f37dd3aa71f27a1a0e34f2f89692a98b99
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-03-27 00:17:18 +0530

    Create find_pivot_index_leetcode.c

---
+++ find_pivot_index_leetcode.c
@@ -0,0 +1,15 @@
+int i,suml=0,sumr=0,r,l;
+    for(i=0;i<n;i++){
+        for(l=0;l<i;l++){
+            suml+=a[l];
+        }
+        for(r=i+1;r<n;r++){
+            sumr+=a[r];
+        }
+        if(sumr-suml==0){
+            return i;
+        }
+        sumr=0;
+        suml=0;
+    }
+    return -1;

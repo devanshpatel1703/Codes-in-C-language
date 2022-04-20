3d18568f58a360a56af39fbd673159f16816ccfc
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-04-20 22:22:53 +0530

    Create Number_of_Steps_to_Reduce_a_Number_to_Zero_leetcode.c

---
+++ Number_of_Steps_to_Reduce_a_Number_to_Zero_leetcode.c
@@ -0,0 +1,15 @@
+int numberOfSteps(int num)
+{
+    int step=0;
+    while(num!=0){
+        if(num%2==0){
+            num/=2;
+            step++;
+        }
+        else{
+            num-=1;
+            step++;
+        }
+    }
+    return(step);
+}

d849633597b67a6f93b419b35936d66a836b823a
devanshpatel1703 <86482120+devanshpatel1703@users.noreply.github.com>
2022-06-17 23:41:10 +0530

    Create Kth Largest Element in an Array.c

---
+++ Kth Largest Element in an Array.c
@@ -0,0 +1,14 @@
+int findKthLargest(int* a, int n, int k)
+{
+    int i,j,temp;
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-1-i;j++){
+            if(a[j]<a[j+1]){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    return (a[k-1]);
+}
